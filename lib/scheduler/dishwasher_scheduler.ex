838f6c9784da1e1b586ac72a77ce682a693df21a
Humberto Rodriguez Avila <rhumbert@vub.ac.be>
2017-09-14 12:38:07 +0200

    Update scheduler messages

--- lib/scheduler/dishwasher_scheduler.ex
+++ lib/scheduler/dishwasher_scheduler.ex
@@ -31,7 +31,7 @@ defmodule Scheduler.DishwasherScheduler do
 
   every :friday, at: "17:00" do
     msg = """
-          We hope you did all your dishwasher duties this week :stuck_out_tongue_winking_eye:
+          Hi Dishwasher Manager! We hope you did all your dishwasher duties this week :stuck_out_tongue_winking_eye:
           Have a nice weekend!
           """
     send_notification(msg)
@@ -39,7 +39,7 @@ defmodule Scheduler.DishwasherScheduler do
 
   every :thursday, at: "14:00" do
     msg = """
-          Hello! Next week you will our Dishwasher Managger :tada:
+          Hello! Next week you will be our Dishwasher Managger :tada:
           If you will be out next week please change your turn with another person using the `swap_with` command.
           e.g. `swap_with @cdetroye`
           """
