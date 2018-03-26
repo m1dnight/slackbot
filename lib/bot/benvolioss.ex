d4a3de4129dbf825a3f894b0b50a5f0141be442f
Christophe De Troyer <christophe@call-cc.be>
2018-03-26 14:12:05 +0200

    prehook benvoliso

--- lib/bot/benvolioss.ex
+++ lib/bot/benvolioss.ex
@@ -2,17 +2,46 @@ defmodule Plugin.Benvolios do
   use Slackbot.Plugin
   require Logger
 
+
   @boss Application.fetch_env!(:slack, :benvolios_owner)
   @channel Application.fetch_env!(:slack, :benvolios_channel)
 
-  def hook_pre(msg) do
-    fmt =
-    if Map.has_key?(msg, :text) do
-      %{msg | text: String.downcase(msg.text)}
+  def hook_pre(msg = %{:text => t}) do
+    # Entire message to lowercase.
+    text = String.downcase(t)
+
+    # Compute all permutations of the first word in the string.
+    # if it matches "order", turn it into "order".
+    words = String.split(text)
+    perms = words |> hd |> String.to_charlist() |> shuffle() |> Enum.map(&String.Chars.to_string/1)
+    match? = Enum.any?(perms, &(String.equivalent?("order", &1)))
+
+    text = if match? do
+      (["order"] ++ (words |> tl)) |> Enum.join(" ")
     else
-      msg
+      text
     end
-    {:ok, fmt}
+
+    # # Lowercase all the message, for easy pattern matching.
+    # fmt =
+    #   if Map.has_key?(msg, :text) do
+    #     lower = String.downcase(msg.text)
+
+    #     # If the first word is a permutation of "order", change it to "order".
+    #     words = String.split(lower)
+    #     matches? = words|> hd |> String.to_charlist() |> shuffle() |> Enum.map(&String.Chars.to_string/1) |> Enum.any?(fn w -> String.equivalent?(w, "order") end)
+
+    #     if matches? do
+    #       text = ["order"] ++ tl(words) |> Enum.join()
+    #       %{msg | text: text}
+    #     else
+    #       %{msg | text: String.downcase(msg.text)}
+    #     end
+    #   else
+    #     msg
+    #   end
+
+    {:ok, %{msg | text: text}}
   end
 
   # The message "order x y z" is used to order a sandwich. "x y z" will be
@@ -108,5 +137,16 @@ defmodule Plugin.Benvolios do
 
   defp handle_order(orderer, order) do
     Brain.Benvolios.save_order(orderer, order)
+  end
+
+  # https://stackoverflow.com/questions/33756396/how-can-i-get-permutations-of-a-list
+  def shuffle(list), do: shuffle(list, length(list))
+  def shuffle([], _), do: [[]]
+  def shuffle(_, 0), do: [[]]
+
+  def shuffle(list, i) do
+    for x <- list,
+        y <- shuffle(list, i - 1),
+        do: [x | y]
   end
 end
